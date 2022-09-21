db1c68e14df852d28483587d209e9575fef0ab82
H3L7BOY <109512295+H3L7BOY@users.noreply.github.com>
2022-09-21 11:07:46 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -32,24 +32,24 @@ global.APIKeys = {
 }
 
 //other
-global.owner = ['94715166712'] // Owner number , (අයිතිකරුගේ නම්බර් එක )
-global.pemilik = ['94715166712'] //Change  it , ඔබට කැමති නම් වෙනස් කරන්න
-global.premium = ['94715166712'] //Change it ඔබට කැමතිනම් වෙනස් කරන්න 
+global.owner = ['5511940098181'] // Owner number , (අයිතිකරුගේ නම්බර් එක )
+global.pemilik = ['5511940098181'] //Change  it , ඔබට කැමති නම් වෙනස් කරන්න
+global.premium = ['5511940098181'] //Change it ඔබට කැමතිනම් වෙනස් කරන්න 
 global.pengguna = 'Dark Maker' // Your name ඔබේ නම 
-global.botnma = '💃 𝐐𝐮𝐞𝐞𝐧 𝐄𝐥𝐢𝐬𝐚 𝐁𝐨𝐭 💃' // Your bot name , ඔබගෙ බොට්ගේ නම 
-global.ownernma = '𝓓𝓪𝓻𝓴 𝓜𝓪𝓴𝓮𝓻' //ownernama,ownername
-global.packname = 'Queen Elisa Sticker' // Sticker package name 
-global.author = 'Dark Maker' // Sticker Autor name 
+global.botnma = 'Lexha' // Your bot name , ඔබගෙ බොට්ගේ නම 
+global.ownernma = 'H3L7BOY' //ownernama,ownername
+global.packname = 'made with lexha' // Sticker package name 
+global.author = 'lexha' // Sticker Autor name 
 global.sessionName = 'session'
 global.prefa = ['#','!','/',''] 
 global.sp = '🔵'
-// Alive massage , ඔබේ බොගේ alive massage  එක මෙහි ඇති ` ` මෙම ලකුනු ඇතුලේ ඔබට අවශ්‍ය ඔනිම දෙයක් Type කරන්න.💃
-global.alivelogo = `https://telegra.ph/file/1a2dee71baa975091620d.jpg`
+// Alive massage , hey alive massage  එක මෙහි ඇති ` ` මෙම ලකුනු ඇතුලේ ඔබට අවශ්‍ය ඔනිම දෙයක් Type කරන්න.💃
+global.alivelogo = `https://i.imgur.com/18ejkaV.jpeg`
 global.alive =`Hello i am alive now
 
 Download song type .yt 
 Thanks fro using...`
-// Welcome massage  ඔබට කැමති නම් මෙයට ඔබෙ welcome massage  එක යොදන්න 
+// Welcome massage  hey welcome massage  එක යොදන්න 
 global.welcome = `
 🌀 Thanks Fro using sl zim bot
 ✾ Some bugs fixing
